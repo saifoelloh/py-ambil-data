31edab9db8d04978c048ddf048753e1cbf6957b6
saifoelloh <saifoelloh@gmail.com>
2019-10-30 05:30:22 +0700

    creating converter db into unnormal

---
+++ normalization.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+myclient = MongoClient('localhost', port=27017, username='mongoadmin', password='admin123')
+mydb = myclient['scrapping2']
+
+myMahasiswa = mydb['mahasiswas']
+myMakul = mydb['makuls']
+myKuliah = mydb['kuliahs']
+myClasses = mydb['classes']
+
+kuliahs = myKuliah.find()
+
+for kuliah in kuliahs:
+    print(kuliah['mahasiswa'])
+    mahasiswa = myMahasiswa.find_one({"_id": kuliah['mahasiswa']})
+    makul = myMakul.find_one({"_id": kuliah['makul']})
+    kelas = {
+            "name": mahasiswa['name'],
+            "nim": mahasiswa['nim'],
+            "ipk": mahasiswa['ipk'],
+            "makul": makul['name'],
+            "code": makul['code'],
+            "status": kuliah['status'],
+    }
+    print(kelas)
+    myClasses.insert_one(kelas)
